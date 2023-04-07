89f09832de70e4a66867bba9cceb518e162b5d38
Akash Singh <furiousakash111@gmail.com>
2023-04-07 11:01:53 +0530

    Made the counter app with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
 function App() {
+  const[count, setCount] = useState(0);
+
+  function decreaseHandler(){
+    setCount(count-1);
+  }
+  function increaseHandler(){
+    setCount(count+1);
+  }
+  function resetHandler(){
+    setCount(0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-[100vw] h-[100vh] flex items-center flex-col justify-center bg-sky-900 gap-5'>
+      <div className='text-xl text-sky-500 font-semibold'>
+        Increment & Decrement
+      </div>
+      <div className='flex gap-5 text-center bg-slate-50 rounded'>
+        <button onClick={decreaseHandler} className='text-4xl border-r-2 border-gray-500 w-16 py-3'>
+          -
+        </button>
+        <div className='text-4xl w-20 py-3'>
+          {count}
+        </div>
+        <button onClick={increaseHandler} className='text-4xl border-l-2 border-gray-500 w-16 py-3'>
+          +
+        </button>
+      </div>
+      <button onClick={resetHandler} className='bg-lime-500 text-lg font-semibold py-2 px-5 rounded-sm'>
+        Reset
+      </button>
     </div>
   );
 }
